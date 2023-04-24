ac891370ec38cebd90ba5c4e3d27045dc1435a4e
SMITH MOGAMAT SHUHAIR <126153069+kxngzero329@users.noreply.github.com>
2023-04-24 14:12:53 +0200

    Add files via upload

---
+++ iwa14/challenge 2/java/javascript.js
@@ -0,0 +1,37 @@
+const add = (a, b) => { return a + b }
+
+const multiply = (a, b) => { return a * b }
+
+function internal() {
+	const added = this.add(this.internal.a, this.internal.b)
+	const multiplied = this.multiply(this.internal.a, this.internal.b)
+	return added * multiplied
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+
+console.log(example1.calculate())
+console.log(example2.calculate())
